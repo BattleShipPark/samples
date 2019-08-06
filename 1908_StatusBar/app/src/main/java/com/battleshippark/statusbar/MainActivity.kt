b2e5ad3474b7fdf8dbeaf71f2b64a540fdd14bd6
BattleShip Park <battleship.park@gmail.com>
2019-08-06 15:30:14 +0900

    Transparent

--- 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/MainActivity.kt
+++ 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.battleshippark.statusbar
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         color_code.setOnClickListener {
             resetWindowFlags()
+
             window.statusBarColor = Color.RED
         }
 
@@ -27,11 +29,34 @@ class MainActivity : AppCompatActivity() {
 
         translucent_code.setOnClickListener {
             resetWindowFlags()
+
             setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
         }
 
         translucent_theme.setOnClickListener {
             startActivity(Intent(this, TranslucentActivity::class.java))
+        }
+
+        transparent1_code.setOnClickListener {
+            resetWindowFlags()
+
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+
+            findViewById<View>(android.R.id.content).requestLayout() // you don't need this if you run above codes before setContentView()
+        }
+
+        transparent2_code.setOnClickListener {
+            resetWindowFlags()
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, 0)
+
+            window?.decorView?.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+
+            findViewById<View>(android.R.id.content).requestLayout() // you don't need this if you run above codes before setContentView()
         }
     }
 
