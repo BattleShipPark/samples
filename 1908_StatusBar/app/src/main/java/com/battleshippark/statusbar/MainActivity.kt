3ec53c56eeb6f1493bc5f92193840c869ec7e852
BattleShip Park <battleship.park@gmail.com>
2019-08-06 14:59:26 +0900

    Translucent

--- 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/MainActivity.kt
+++ 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/MainActivity.kt
@@ -3,22 +3,40 @@ package com.battleshippark.statusbar
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var defaultFlag: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        defaultFlag = window.attributes.flags
+
         color_code.setOnClickListener {
+            resetWindowFlags()
             window.statusBarColor = Color.RED
         }
 
         color_theme.setOnClickListener {
             startActivity(Intent(this, ColorActivity::class.java))
         }
+
+        translucent_code.setOnClickListener {
+            resetWindowFlags()
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+
+        translucent_theme.setOnClickListener {
+            startActivity(Intent(this, TranslucentActivity::class.java))
+        }
+    }
+
+    private fun resetWindowFlags() {
+        window.attributes.flags = defaultFlag
     }
 
     private fun setWindowFlag(bits: Int, on: Boolean) {
---
+++ 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/TranslucentActivity.kt
@@ -0,0 +1,12 @@
+package com.battleshippark.statusbar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TranslucentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
