4585989f5b99d86017a8f001613ffa9417fbeee4
BattleShip Park <battleship.park@gmail.com>
2019-08-06 14:36:39 +0900

    StatusBar

---
+++ 1908_StatusBar/app/src/main/java/com/battleshippark/statusbar/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.battleshippark.statusbar
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        color_code.setOnClickListener {
+            window.statusBarColor = Color.RED
+        }
+
+        color_theme.setOnClickListener {
+            startActivity(Intent(this, ColorActivity::class.java))
+        }
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window ?: return
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+}
