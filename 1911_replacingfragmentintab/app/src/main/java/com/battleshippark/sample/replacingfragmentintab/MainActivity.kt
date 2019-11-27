270384a0f0ab325952a4e13ca34d511264070a1e
BattleShip Park <battleship.park@gmail.com>
2019-11-27 17:14:47 +0900

    Add FragmentTransactionActivity

--- 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/MainActivity.kt
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.battleshippark.sample.replacingfragmentintab
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.battleshippark.sample.replacingfragmentintab.ft.FragmentTransactionActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickFragmentTransaction(view: View) {
+        startActivity(Intent(this, FragmentTransactionActivity::class.java))
     }
 }
---
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft/FragmentTransactionActivity.kt
@@ -0,0 +1,13 @@
+package com.battleshippark.sample.replacingfragmentintab.ft
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.battleshippark.sample.replacingfragmentintab.R
+
+class FragmentTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_transaction)
+    }
+}
