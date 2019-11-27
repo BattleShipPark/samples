fae6bba283904629135389e7269ed4803bdcc289
BattleShip Park <battleship.park@gmail.com>
2019-11-27 18:02:35 +0900

    Use FragmentTransaction.add()/show()/hide()

--- 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft/FragmentTransactionActivity.kt
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft/FragmentTransactionActivity.kt
@@ -7,27 +7,66 @@ import com.battleshippark.sample.replacingfragmentintab.R
 import kotlinx.android.synthetic.main.activity_fragment_transaction.*
 
 class FragmentTransactionActivity : AppCompatActivity() {
-    private val fragment1: Fragment by lazy { Fragment1() }
-    private val fragment2: Fragment by lazy { Fragment2() }
-    private val fragment3: Fragment by lazy { Fragment3() }
+    private var fragment1: Fragment? = null
+    private var fragment2: Fragment? = null
+    private var fragment3: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_transaction)
 
         tab1.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment1)
-                .commit()
+            supportFragmentManager.beginTransaction().run {
+                if (fragment1 == null) {
+                    fragment1 = Fragment1()
+                    add(R.id.fragment_container, fragment1!!)
+                } else {
+                    show(fragment1!!)
+                }
+                if (fragment2 != null) {
+                    hide(fragment2!!)
+                }
+                if (fragment3 != null) {
+                    hide(fragment3!!)
+                }
+                commit()
+            }
         }
 
         tab2.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment2)
-                .commit()
+            supportFragmentManager.beginTransaction().run {
+                if (fragment2 == null) {
+                    fragment2 = Fragment2()
+                    add(R.id.fragment_container, fragment2!!)
+                } else {
+                    show(fragment2!!)
+                }
+                if (fragment1 != null) {
+                    hide(fragment1!!)
+                }
+                if (fragment3 != null) {
+                    hide(fragment3!!)
+                }
+                commit()
+            }
         }
 
         tab3.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment3)
-                .commit()
+            supportFragmentManager.beginTransaction().run {
+                if (fragment3 == null) {
+                    fragment3 = Fragment3()
+                    add(R.id.fragment_container, fragment3!!)
+                } else {
+                    show(fragment3!!)
+                }
+                if (fragment1 != null) {
+                    hide(fragment1!!)
+                }
+                if (fragment2 != null) {
+                    hide(fragment2!!)
+                }
+                commit()
+            }
         }
     }
 }
