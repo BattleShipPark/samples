018540f250aacef722761144e83ad040aa5a2415
BattleShip Park <battleship.park@gmail.com>
2019-11-27 17:37:58 +0900

    Add fragment1

--- 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft/FragmentTransactionActivity.kt
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft/FragmentTransactionActivity.kt
@@ -2,12 +2,20 @@ package com.battleshippark.sample.replacingfragmentintab.ft
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.battleshippark.sample.replacingfragmentintab.R
+import kotlinx.android.synthetic.main.activity_fragment_transaction.*
 
 class FragmentTransactionActivity : AppCompatActivity() {
+    private val fragment1: Fragment by lazy { Fragment1() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_transaction)
+
+        tab1.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment1)
+                .commit()
+        }
     }
 }
