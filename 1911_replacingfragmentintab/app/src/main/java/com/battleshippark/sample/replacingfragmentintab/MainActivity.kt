def1d118a3ec19cadf61c52a486289a850f6e378
BattleShip Park <battleship.park@gmail.com>
2019-11-28 10:20:53 +0900

    Add ViewPagerFragmentTransactionActivity

--- 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/MainActivity.kt
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.battleshippark.sample.replacingfragmentintab.ft1.FragmentTransactionActivity
+import com.battleshippark.sample.replacingfragmentintab.ft2.ViewPagerFragmentTransactionActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +19,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickFragmentTransaction2(view: View) {
+        startActivity(Intent(this, ViewPagerFragmentTransactionActivity::class.java))
     }
 }
---
+++ 1911_replacingfragmentintab/app/src/main/java/com/battleshippark/sample/replacingfragmentintab/ft2/ViewPagerFragmentTransactionActivity.kt
@@ -0,0 +1,13 @@
+package com.battleshippark.sample.replacingfragmentintab.ft2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.battleshippark.sample.replacingfragmentintab.R
+
+class ViewPagerFragmentTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager_fragment_transaction)
+    }
+}
